4f26839113d04717fdcf2182581c1dd4399a1f05
truong minh phung <c.truong.phung@zonarsystems.com>
2025-03-30 17:06:55 +0700

    create login api

---
+++ backend/utils/auth.py
@@ -0,0 +1,15 @@
+import jwt
+import time
+from constants.app_constants import app_constants
+
+
+def generate_jwt(payload, secret) -> str:
+    """
+    Generates a JWT token with a 24-hour expiration time.
+    """
+    # Set the expiration time to 24 hours from now
+    expiration_time = int(time.time()) + app_constants.JWT_EXPIRATION_TIME # 24 hours in seconds
+    payload["exp"] = expiration_time
+    # Generate the JWT token
+    token = jwt.encode(payload, secret, algorithm=app_constants.JWT_ALGORITHM)
+    return token
---
+++ backend/utils/config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config:
+    """
+    Configuration class for the application.
+    """
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_PORT = os.environ.get("DB_PORT", "5432")  # Default to 5432 if not set
+
+    JWT_SECRET = os.environ.get("JWT_SECRET")
+
+
+    @classmethod
+    def get_db_connection_string(cls):
+        """
+        Returns the database connection string.
+        """
+        return f"postgresql://{cls.DB_USER}:{cls.DB_PASSWORD}@{cls.DB_HOST}:{cls.DB_PORT}/{cls.DB_NAME}"
+    
+    @classmethod
+    def get_jwt_secret(cls):
+        """
+        Returns the JWT secret key.
+        """
+        return cls.JWT_SECRET
+    
+
+# create a singleton instance of the Config class
+config = Config()
